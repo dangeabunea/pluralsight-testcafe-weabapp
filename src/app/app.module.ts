fb06b4df90ea0089f122f4fa4445ba0fbd73147d
Dan Geabunea <dangeabunea@hotmail.com>
2020-10-10 14:24:56 +0300

    Added login/logout features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { FeedbackComponent } from './feedback/feedback.component';
-import { NotesComponent } from './notes/notes.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import {Router, RouterModule, Routes} from "@angular/router";
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
-import { NoteComponent } from './notes/note/note.component';
-import { NoteTextFilterPipe } from './shared/note-text-filter.pipe';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {FeedbackComponent} from './feedback/feedback.component';
+import {NotesComponent} from './notes/notes.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {NoteComponent} from './notes/note/note.component';
+import {NoteTextFilterPipe} from './shared/note-text-filter.pipe';
+import {LoginComponent} from './login/login.component';
+import {AuthenticatedGuard} from './shared/authenticated.guard';
 
-const appRoutes :Routes = [
+const appRoutes: Routes = [
   {
-    path:'notes',
-    component:NotesComponent
+    path: 'notes',
+    component: NotesComponent,
+    canActivate: [AuthenticatedGuard]
   },
   {
-    path:'feedback',
-    component:FeedbackComponent
+    path: 'feedback',
+    component: FeedbackComponent,
+    canActivate: [AuthenticatedGuard]
   },
   {
-    path:'',
-    component:NotesComponent,
-    pathMatch:'full'
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full'
   },
   {
-    path:'**',
-    component:NotFoundComponent
+    path: '',
+    component: NotesComponent,
+    pathMatch: 'full',
+    canActivate: [AuthenticatedGuard]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
@@ -40,15 +50,18 @@ const appRoutes :Routes = [
     NotesComponent,
     NotFoundComponent,
     NoteComponent,
-    NoteTextFilterPipe
+    NoteTextFilterPipe,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes, {enableTracing:true})
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from '../shared/login.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _loginService: LoginService, private _router: Router) {
   }
 
+  public ngOnInit(): void {
+  }
+
+  public get isAuthenticated(): boolean {
+    return this._loginService.isAuthenticated;
+  }
+
+  public get userName(): string {
+    return this._loginService.userName;
+  }
+
+  public get userIsPremiumMember(): boolean {
+    return this._loginService.hasPremiumSubscription;
+  }
+
+  public signOut(): void {
+    this._loginService.signOut();
+    this._router.navigate(['login']);
+  }
 }
